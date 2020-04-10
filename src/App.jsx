a007368e99f47f1fad56815e58f91a71dc69a245
wisnuwicaks <wisnucaks@gmail.com>
2020-04-11 00:53:54 +0700

    oke

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import NewScreen from "./views/components/NewScreen"
-import TableProduct from "./views/components/TableProduct"
-import style from "./style.css"
+import NewScreen from "./views/components/NewScreen";
+import TableProduct from "./views/components/TableProduct";
+import style from "./style.css";
+import CounterScreen from "./views/screen/CounterScreen";
+import ProductCard from "./views/components/ProductCard"
+
 function App() {
 
- 
+//  let arr = [1,2,3]
+//  const renderArr = ()=>{
+//    return (arr.map((val)=>{
+//       return <CounterScreen cek={val}/>
+//    }))
+//  }
+let arrProduct =[
+{
+  nama: "Sepatu Basket",
+  harga: 2300000,
+  desc: "Sepatu basket keluaran terbaru loh! Pake sepatu ini auto menang",
+  disc:20,
+  stok : 2
+},
+{
+  nama: "Kaos Polo",
+  harga: 250000,
+  desc: "Tingkatkan penampilanmu menggunakan kaos berkelas ini",
+  disc :40,
+  stok : 0
+},
+{
+  nama: "Celana Jeans",
+  harga: 550000,
+  desc: "Nyaman dan dengan bahan premium gan, stock selalu ready!",
+  disc :0,
+  stok : 3
+}
+]
+
+const renderData = ()=>{
+ return  (arrProduct.map((val)=>{
+    return <ProductCard productData={val}/>
+  })
+ )
+}
+
   return (
     <div className="App">
-    <TableProduct/>
+      {
+        renderData()
+      }
     </div>
   );
 }
---
+++ src/views/components/ProductCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ProductCard =(props)=>{
+    //disctructuring
+    const {productData} = props
+    const {nama, harga, desc,disc,stok} = productData
+    const renderingItems =()=>{
+        if(stok){
+            return <>
+            <h1>ProductCard</h1>
+            <h3>Nama Produk : {nama}</h3>
+            <h3>Harga : {harga}</h3>
+            {
+            disc>0?<h3>Harga Diskon : {harga*((100-disc)/100)}</h3>:null
+            }
+            <h3>Deskripsi : {desc}</h3>
+            </>
+        }
+        else{
+            return <h3>Item Kosong </h3>
+        }
+    }
+    return (
+        <div
+            style={{
+                border:"1px solid black",
+                width:"240px",
+                borderRadius:"7px",
+                padding :"10px"
+            }}
+        >
+            {renderingItems()}
+        </div>
+    )
+}
+
+export default ProductCard
